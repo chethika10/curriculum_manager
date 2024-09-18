88631eb40e7a0cc5b52b616476f1096f274266d5
chethika10 <chethika10@gmail.com>
2024-09-18 12:05:40 +0530

    initialize project

---
+++ src/main/java/com/uom/curriculum_manager/CurriculumManagerApplication.java
@@ -0,0 +1,13 @@
+package com.uom.curriculum_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumManagerApplication.class, args);
+	}
+
+}
