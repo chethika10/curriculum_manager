06e11b922f54cf98373bf3c0dc29edc15dfdb3f6
chethika10 <chethika10@gmail.com>
2024-10-23 17:29:23 +0530

    Update AddModule.jsx

--- curriculum_manager_ui/react_app/src/Screens/AddModule.jsx
+++ curriculum_manager_ui/react_app/src/Screens/AddModule.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AddModule = () => {
+  const [code, setCode] = useState("");
+  const [title, setTitle] = useState("");
+  const [isgpa, setGpa] = useState(0);
+  const [lectureHours, setLectureHours] = useState(0);
+  const [labHours, setLabHours] = useState(0);
+  const [credits, setCredits] = useState(0);
+  const [EvaluationCa, setEvaluationCa] = useState(0);
+  const [objectives, setObjectives] = useState("");
+
+  // useEffect(() => {
+  //   console.log (isgpa)
+
+  // }, [isgpa])
+
   return (
     <div className="back">
       Add Module
@@ -11,22 +25,36 @@ const AddModule = () => {
         <label htmlFor="code" className="form-label">
           Module code
         </label>
-        <input type="text" className="form-control bg-transparent" id="code" />
+        <input
+          onChange={(e) => setCode(e.target.value)}
+          type="text"
+          className="form-control bg-transparent"
+          id="code"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="title" className="form-label">
           Module title
         </label>
-        <input type="text" className="form-control bg-transparent" id="title" />
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          className="form-control bg-transparent"
+          id="title"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="isgpa" className="form-label">
           GPA/NGPA
         </label>
         <br />
-        <select name="isgpa" id="isgpa">
-          <option value="1">GPA</option>
-          <option value="2">NGPA</option>
+        <select
+          onChange={(e) => setGpa(e.target.value)}
+          name="isgpa"
+          id="isgpa"
+        >
+          <option value="0">GPA</option>
+          <option value="1">NGPA</option>
         </select>
       </div>
       <div className="mb-3">
@@ -34,6 +62,7 @@ const AddModule = () => {
           Lecture Hours per week
         </label>
         <input
+          onChange={(e) => setLectureHours(e.target.value)}
           type="number"
           className="form-control bg-transparent"
           id="lecHours"
@@ -44,6 +73,7 @@ const AddModule = () => {
           Lab/Tutorial Hours per week
         </label>
         <input
+          onChange={(e) => setLabHours(e.target.value)}
           type="number"
           className="form-control bg-transparent"
           id="labHours"
@@ -54,6 +84,7 @@ const AddModule = () => {
           Credits
         </label>
         <input
+          onChange={(e) => setCredits(e.target.value)}
           type="number"
           className="form-control bg-transparent"
           id="credits"
@@ -73,6 +104,7 @@ const AddModule = () => {
       <div className="input-group ">
         <span className="input-group-text bg-transparent">CA</span>
         <input
+          onChange={(e) => setEvaluationCa(e.target.value)}
           type="number"
           aria-label="CA"
           className="form-control bg-transparent"
@@ -82,6 +114,8 @@ const AddModule = () => {
           type="number"
           aria-label="WE"
           className="form-control bg-transparent"
+          disabled
+          value={100 - EvaluationCa}
         />
       </div>
       <div className="mb-3">
@@ -89,6 +123,7 @@ const AddModule = () => {
           Module Objectives
         </label>
         <textarea
+          onChange={(e) => setObjectives(e.target.value)}
           className="form-control bg-transparent"
           id="objectives"
           rows="3"
