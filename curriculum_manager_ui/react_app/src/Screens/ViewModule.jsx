d3cd4ab001559aab698c9d5a687494fd9116d13a
chethika10 <chethika10@gmail.com>
2024-11-12 19:30:07 +0530

    Update ViewModule.jsx

--- curriculum_manager_ui/react_app/src/Screens/ViewModule.jsx
+++ curriculum_manager_ui/react_app/src/Screens/ViewModule.jsx
@@ -10,6 +10,11 @@ const ViewModule = () => {
     gpa: true,
     lectureHours: 0,
     labHours: 0,
+    credits: 0,
+    evaluationCa: 0,
+    objectives: "",
+    learningOutcomes: [],
+    syllabusOutlines: [],
   });
   const location = useLocation();
   const index = location.state.moduleCode;
@@ -99,6 +104,134 @@ const ViewModule = () => {
           className="form-control bg-transparent"
           id="labHours"
         />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="credits" className="form-label">
+          Credits
+        </label>
+        <input
+          // onChange={(e) => setCredits(e.target.value)}
+          disabled
+          value={module.credits}
+          type="number"
+          className="form-control bg-transparent"
+          id="credits"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="credits" className="form-label">
+          pre-requisites
+        </label>
+        <input
+          type="number"
+          className="form-control bg-transparent"
+          id="credits"
+        />
+      </div>
+      <label className="form-label">Evaluation</label>
+      <div className="input-group ">
+        <span className="input-group-text bg-transparent">CA</span>
+        <input
+          // onChange={(e) => setEvaluationCa(e.target.value)}
+          disabled
+          value={module.evaluationCa}
+          type="number"
+          aria-label="CA"
+          className="form-control bg-transparent"
+        />
+        <span className="input-group-text bg-transparent">WE</span>
+        <input
+          type="number"
+          aria-label="WE"
+          className="form-control bg-transparent"
+          disabled
+          value={100 - module.evaluationCa}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="objectives" className="form-label">
+          Module Objectives
+        </label>
+        <textarea
+          // onChange={(e) => setObjectives(e.target.value)}
+          disabled
+          value={module.objectives}
+          className="form-control bg-transparent"
+          id="objectives"
+          rows="3"
+        ></textarea>
+      </div>
+      <div>
+        <label htmlFor="los" className="form-label">
+          Learning outcomes
+        </label>
+        <br />
+        {module.learningOutcomes.map((data, i) => {
+          return (
+            <div key={i} className="mb-3">
+              <label htmlFor={i} className="form-label">
+                LO{i + 1}
+              </label>
+              <textarea
+                value={data.learningOutcome}
+                // onChange={(e) => handleLochange(e, i)}
+                disabled
+                className="form-control bg-transparent"
+                id={i}
+                rows="3"
+              ></textarea>
+              <br />
+            </div>
+          );
+        })}
+        <br />
+      </div>
+      <div>
+        <label htmlFor="los" className="form-label">
+          Syllabus Outlines
+        </label>
+        <br />
+        {module.syllabusOutlines.map((data, i) => {
+          return (
+            <div key={i} className="mb-3">
+              <label htmlFor="a" className="form-label">
+                Syllabus Outline 1
+              </label>
+              <textarea
+                value={data.syllabusOutline}
+                // onChange={(e) => handleSoValuechange(e, i)}
+                disabled
+                className="form-control bg-transparent"
+                id="a"
+                rows="3"
+              ></textarea>
+              <label htmlFor="a" className="form-label">
+                description
+              </label>
+              <textarea
+                value={data.description}
+                // onChange={(e) => handleSoDescriptionchange(e, i)}
+                disabled
+                className="form-control bg-transparent"
+                id="a"
+                rows="3"
+              ></textarea>
+              <label htmlFor="hours" className="form-label">
+                Hours
+              </label>
+              <input
+                value={data.hours}
+                // onChange={(e) => handleSoHourschange(e, i)}
+                disabled
+                type="number"
+                className="form-control bg-transparent"
+                id="hours"
+              />
+              <br />
+            </div>
+          );
+        })}
+        <br />
       </div>
     </div>
   );
